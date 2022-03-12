0fe11d60d0a34bf1851aad91a8d162a1d4208dc6
Brian Marth <marthbrian@gmail.com>
2022-03-12 12:16:18 -0500

    forms1

---
+++ src/components/form.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+
+const UserForm = (props)=>{
+    const[username, setUsername] = useState("");
+    const[email, setEmail] = useState("");
+    const[password, setPassword] = useState("");
+
+    const createUser = (e) => {
+        e.preventDefault();
+        const newUser = {username, email, password};
+        console.log ("Welcome", newUser);
+    };
+
+    return(
+        <div>
+        <form onSubmit = {createUser}>
+            <div>
+                <label>Username: </label>
+                <input type="text" onChange= {(e) => setUsername(e.target.value)}/>
+            </div>
+            <div>
+                <label>Email: </label>
+                <input type="text" onChange= {(e) => setEmail(e.target.value)}/>
+            </div>
+            <div>
+                <label>Password: </label>
+                <input type="text" onChange= {(e) => setPassword(e.target.value)}/>
+            </div>
+            <input type="submit" value="Create User" />
+        </form>
+
+        <p>{username}</p>
+        <p>{email}</p>
+        <p>{password}</p>
+        </div>
+    );
+};
+export default UserForm;
